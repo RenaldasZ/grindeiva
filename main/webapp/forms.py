3cf4552b7107fe5fee4e7279fac230ac65460b75
kobrius11 <kobrius11@gmail.com>
2024-01-10 13:09:58 +0200

    webapp views, contact form setup stage

---
+++ main/webapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(label='subject', max_length=255, required=True)
+    full_name = forms.CharField(label='full_name', max_length=255, required=True)
+    email = forms.EmailField(label='email', required=True)
+    question = forms.CharField(label='question', max_length=2000, required=True)
+
+    class Meta:
+        fields = ('subject', 'full_name', 'email', 'question')
+    
