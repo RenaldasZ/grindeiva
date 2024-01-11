833b8c9777073a782eaa7124672652e9f38d3d82
RenaldasZ <spikeris2@gmail.com>
2024-01-11 16:17:00 +0200

    created additional style

--- main/webapp/static/webapp/js/ui.js
+++ main/webapp/static/webapp/js/ui.js
@@ -15,3 +15,12 @@ function updateValue() {
     let calculation = (width * (thickness / 12))/ 27;
     cost.textContent = calculation;
 }
+
+async function active_button(obj_id) {
+    const uncollapsible = document.getElementById(obj_id);
+    if (uncollapsible.classList.contains("active")) {
+        uncollapsible.classList.remove("active");
+    } else {
+        uncollapsible.classList.add("active");
+    };
+};
