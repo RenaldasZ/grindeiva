85c6544e662e2f5b0aece0dbdce4994adac9b5ad
RenaldasZ <spikeris2@gmail.com>
2024-01-17 20:31:08 +0200

    frontend html page styles updated added about us html

--- main/webapp/urls.py
+++ main/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import Index, GaleryView, ContactsView, CalculatorView
+from . views import Index, About, GaleryView, ContactsView, CalculatorView
 
 urlpatterns = [
     path('', Index.as_view(), name="index"),
     path('galery/', GaleryView.as_view(), name="galery"),
     path('contacts/', ContactsView.as_view(), name="contacts"),
     path('calculator/', CalculatorView.as_view(), name="calculator"),
+    path('about/', About.as_view(), name="about"),
 ]
